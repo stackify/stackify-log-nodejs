42419cd6b4a4401947da9a686861f934d4417402
alexk-blackops <Alex@Alexanders-MacBook-Pro.local>
2014-11-24 15:00:52 +0300

    scheduled delay changed

--- config/config.js
+++ config/config.js
@@ -1,20 +1,27 @@
 module.exports = {
-    'PROTOCOL' : 'https',
-    'HOST': 'dev.stackify.com',
-    'PORT': 443,
-    'IDENTIFY_PATH': '/API/Metrics/IdentifyApp',
-    'LOG_SAVE_PATH': '/API/Log/Save',
-    'MSG_LIMIT': 100, // number of messages per batch
-    'MSG_CAP': 10000, // overall messages cap in the queue
-    'SCAN_TIMER': 30 * 1000,
-    'REQUEST_TIMER': 5 * 1000,
-    'REQUEST_ATTEMPTS': 5, // number of attempts if API call isn't succesful
-    /**
-     * Number of instances of a unique error that are allowed to be sent in one minute 
-     */
-    'MAX_DUP_ERROR_PER_MINUTE': 5,
+    PROTOCOL               : 'https',
+    HOST                   : 'dev.stackify.com',
+    PORT                   : 443,
+    IDENTIFY_PATH          : '/API/Metrics/IdentifyApp',
+    LOG_SAVE_PATH          : '/API/Log/Save',
+    // possible delay values in millis
+    DELAY: {
+        ONE_SECOND_DELAY   : 1000,
+        FIVE_SECONDS_DELAY : 5000,
+        ONE_MINUTE_DELAY   : 60000,
+        FIVE_MINUTES_DELAY : 300000,
+    },
+    MSG: {
+        QUEUE_CAP          : 10000, // overall messages cap in the queue
+        // minimum and maximum batch sizes
+        MIN_BATCH_SIZE     : 10,
+        MAX_BATCH_SIZE     : 100,
+        // Number of instances of a unique error that are allowed to be sent in one minute 
+        MAX_DUP_ERROR_PER_MINUTE: 5,
+    }, 
+    IDENTIFY_DELAY: 300000, // 5 minutes delay if identifyApp call failed
     /**
      * Cookie value 
      */
-    'COOKIE_MASK': 'X-MASKED-X'
+    COOKIE_MASK: 'X-MASKED-X'
 };
--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 var http = require('http');
 var util = require('util');
+var winston = require('winston');
 var stackify = require('./index');
-stackify.start({apiKey: '0Zw8Fj4Hr3Aa1Sf2Gw4Cb3Gk7Fp6Zn6Sc0Gw2Cr', env: 'dev', proxy: 'http://89.22.50.155:3128'});
 var stack = require('stack-trace');
 var pkginfo = require('pkginfo')(module, 'name');
 var express = require("express");
@@ -9,16 +9,27 @@ var app = express();
 var bodyParser = require('body-parser');
 var port = process.env.PORT || 5000;
 
+stackify.start({apiKey: '0Zw8Fj4Hr3Aa1Sf2Gw4Cb3Gk7Fp6Zn6Sc0Gw2Cr', env: 'dev'/*, proxy: 'http://89.22.50.155:3128'*/});
+require('./new');
+winston.add(winston.transports.Stackify, {level: 'silly', storage: stackify});
 
 var foo = function foo() {
-    stackify.warn('sdfg', {dfgh: 45, gh: 67});
-    stackify.error('dfg');
+/*    stackify.warn('sdfg', {dfgh: 45, gh: 67});
+    stackify.error('dfg');*/
+    for (var i = 2345; i >= 0; i--) {
+        stackify.warn('sdfg');
+    };
+    setTimeout(function() {
+      for (var i = 3345; i >= 0; i--) {
+        winston.info('info');
+      };  
+    }, 10000);
 };
 foo();
 
-setInterval(function () {
+/*setInterval(function () {
     stackify.error('test');
-}, 1500);
+}, 1500);*/
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
